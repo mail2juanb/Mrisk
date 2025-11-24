700774f80832a7604d327e4bf28cebeea5ce431e
juanb <mail2juanb@yahoo.fr>
2025-11-25 00:15:12 +0100

    Add Eureka, Actuator, Zipkin, App informations

--- src/main/java/com/microdiab/mrisk/MriskApplication.java
+++ src/main/java/com/microdiab/mrisk/MriskApplication.java
@@ -2,8 +2,10 @@ package com.microdiab.mrisk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MriskApplication {
 
     public static void main(String[] args) {
