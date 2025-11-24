faae151484727ba286c97f2e0dae99d48f51c4cd
juanb <mail2juanb@yahoo.fr>
2025-11-24 23:53:02 +0100

    Initial commit

---
+++ src/main/java/com/microdiab/mrisk/MriskApplication.java
@@ -0,0 +1,13 @@
+package com.microdiab.mrisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MriskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MriskApplication.class, args);
+    }
+
+}
